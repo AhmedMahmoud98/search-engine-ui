df6e468aeb28e40cf5f2d0a0d721d5ce5fbc3ddf
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2020-05-06 04:55:02 +0200

    add Search Results Component

---
+++ src/app/model/page.ts
@@ -0,0 +1,12 @@
+/**
+ * Result : It represents a website page  
+ * that retrieve from the back end
+ */
+export interface Page {
+    /** The page title */
+    title: string;
+    /** The page URL */
+    url: string;
+    /** The page summary */
+    summary: string;
+}

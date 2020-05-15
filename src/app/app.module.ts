947d3fb08bc59597bf05d42764311c05280edb33
phella <philopateernabil2021@gmail.com>
2020-05-15 12:58:48 +0200

    add import groups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from '../app/services/in-memory-data-service.service';
-import { HomePageComponent } from './home-page/home-page.component';
-import { SearchBoxComponent } from './search-box/search-box.component';
 import { RouterModule,Routes } from '@angular/router';
-import { SearchResultsTextComponent } from './search-results-text/search-results-text.component';
-import { SearchResultsImagesComponent } from './search-results-images/search-results-images.component';
-import { TrendsHistogramComponent } from './trends-histogram/trends-histogram.component'
 import { ChartsModule } from 'ng2-charts';
 import { FormsModule } from '@angular/forms';
 import { NgxFlagPickerModule } from 'ngx-flag-picker';
+
+// Http Services
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from '../app/services/in-memory-data-service.service';
+
+// Components
+import { SearchResultsTextComponent } from './search-results-text/search-results-text.component';
+import { SearchResultsImagesComponent } from './search-results-images/search-results-images.component';
+import { TrendsHistogramComponent } from './trends-histogram/trends-histogram.component'
+import { AppComponent } from './app.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 
 const routes: Routes = [{ path: '', component: HomePageComponent },
